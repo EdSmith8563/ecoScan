060588f58b9c0387896de3763f311f9c0864e44f
EdSmith8563 <edsmith.uk4@gmail.com>
2024-02-23 18:01:21 +0000

    UserProfile

--- accounts/models.py
+++ accounts/models.py
@@ -1,31 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import User, Group, Permission
 from map.models import Location
 
-class CustomUser(AbstractUser):
-    # Add additional fields if needed
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     total_points = models.IntegerField(default=0)
-
-    groups = models.ManyToManyField(
-        Group,
-        verbose_name='groups',
-        blank=True,
-        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
-        related_name="customuser_set",
-        related_query_name="user",
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        verbose_name='user permissions',
-        blank=True,
-        help_text='Specific permissions for this user.',
-        related_name="customuser_set",
-        related_query_name="user",
-    )
+    def __str__(self):
+        return self.user
+    
 
 
 class UserLocation(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     points_obtained = models.IntegerField(default=0)
     questions_answered_right = models.IntegerField(default=0)
