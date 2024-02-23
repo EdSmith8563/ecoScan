81dbe9d3373749aacf0f53aac539abab3383e263
EdSmith8563 <edsmith.uk4@gmail.com>
2024-02-23 18:08:31 +0000

    account removal

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     total_points = models.IntegerField(default=0)
     def __str__(self):
-        return self.user
+        return self.user.username
     
 
 
