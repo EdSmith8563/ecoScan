b96e66250bc86a3393beca66c293f4883ab68b50
sbayley1 <130076475+sbayley1@users.noreply.github.com>
2024-02-23 20:10:23 +0000

    changed settings.py and camera.html

--- ecoScan/settings.py
+++ ecoScan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@7l=w2fm1#he=3v6pb3i56@0js6g+tw1y%=1sep214=x+lj%rs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0','192.168.0.29']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0','192.168.0.29','192.168.0.111','192.168.0.188']
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'map',
     'accounts',
     "django_extensions",
+    'qr_code',
 ]
 
 MIDDLEWARE = [
