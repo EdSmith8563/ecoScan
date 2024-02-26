49281ec38978463e1fb2b95c815f5d218e023d5c
EdSmith8563 <edsmith.uk4@gmail.com>
2024-02-26 11:57:48 +0000

    quiz

--- camera/urls.py
+++ camera/urls.py
@@ -3,4 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.camera, name='camera'),
+    path('quiz/', views.quiz1, name='quiz1'),
+    path('quiz/', views.quiz2, name='quiz2'),
+    path('quiz/', views.quiz3, name='quiz3'),
+    path('quiz/', views.quiz4, name='quiz4'),
+    path('quiz/', views.quiz5, name='quiz5'),
+    path('quiz/', views.quiz6, name='quiz6'),
+    path('quiz/', views.quiz7, name='quiz7'),
+    path('quiz/', views.quiz8, name='quiz8'),
+    path('quiz/', views.quiz9, name='quiz9'),
+    path('quiz/', views.quiz10, name='quiz10'),
+    path('quiz/', views.quiz11, name='quiz11'),
+    path('quiz/', views.quiz12, name='quiz12'),
 ]
--- camera/views.py
+++ camera/views.py
@@ -2,3 +2,45 @@ from django.shortcuts import render
 
 def camera(request):
     return render(request, 'camera.html')
+def quiz1(request):
+    # Your view logic
+    return render(request, 'quiz/quiz1.html')
+
+def quiz2(request):
+    # Your view logic
+    return render(request, 'quiz/quiz2.html')
+def quiz3(request):
+    # Your view logic
+    return render(request, 'quiz/quiz3.html')
+
+def quiz4(request):
+    # Your view logic
+    return render(request, 'quiz/quiz4.html')
+def quiz5(request):
+    # Your view logic
+    return render(request, 'quiz/quiz5.html')
+
+def quiz6(request):
+    # Your view logic
+    return render(request, 'quiz/quiz6.html')
+def quiz7(request):
+    # Your view logic
+    return render(request, 'quiz/quiz7.html')
+
+def quiz8(request):
+    # Your view logic
+    return render(request, 'quiz/quiz8.html')
+def quiz9(request):
+    # Your view logic
+    return render(request, 'quiz/quiz9.html')
+
+def quiz10(request):
+    # Your view logic
+    return render(request, 'quiz/quiz10.html')
+def quiz11(request):
+    # Your view logic
+    return render(request, 'quiz/quiz11.html')
+
+def quiz12(request):
+    # Your view logic
+    return render(request, 'quiz/quiz12.html')
